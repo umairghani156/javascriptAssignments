b26dc4c1104a342912987ab78dc292ef0e428c26
umair <umairghani156@gamil.com>
2023-09-09 17:51:03 +0500

    Js Assignment 2

---
+++ app2.js
@@ -0,0 +1,33 @@
+//Q no 1
+var username;
+//Q no 2
+var myName = "Umair Ahmed"
+//Q no 3
+var message;
+message="Hello world"
+alert(message)
+//Q no 4
+var myName;
+myName="Umair Ahmed"
+var myAge;
+myAge="21 years old"
+var myComment;
+myComment="Certified Mobile Application Development"
+alert(myName)
+alert(myAge)
+alert(myComment)
+//Q no 5
+var comments;
+comments="PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(comments)
+//Q no# 6
+var email= "umairghani06@gmail.com";
+alert("My email address is " + email)
+//Question 7
+var book = "A smarter way to learn JavaScript";
+alert(book)
+//Question 8
+document.write("Yah! I can write HTML content through Javascript")
+// Q no# 9
+var symbol = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(symbol)
